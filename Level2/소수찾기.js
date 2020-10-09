b46c8944026343425c71f787b0d09720098db173
taeheongo <taeheon9966@icloud.com>
2020-10-09 18:31:51 +0900

    prime number

---
+++ Level2/소수찾기.js
@@ -0,0 +1,31 @@
+function solution(numbers) {
+  var answer = 0;
+
+  let possibleNumbers = new Set();
+
+  function findNumbers(str, possible, start) {
+    // 가능한 모든 수를 배열로 반환.
+    for (let i = 0; i < str.length; i++) {
+      possible.add(Number(start + str[i]));
+      let tmp_str = str.slice(0, i) + str.slice(i + 1, str.length);
+      findNumbers(tmp_str, possible, start + str.slice(i, i + 1));
+    }
+  }
+
+  findNumbers(numbers, possibleNumbers, "");
+
+  for (let num of possibleNumbers.values()) {
+    console.log(num);
+    let count = 0;
+    for (let n = 1; n <= num; n++) {
+      if (num % n === 0) {
+        count++;
+      }
+    }
+    if (count === 2) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
